6e98115a08a4e5177bfe49f0685fcca3b76377d1
brunopradoprado <bruno.2006p@gmail.com>
2026-04-16 11:37:41 -0300

    inicio migrate props -> context

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,30 +9,17 @@ import Categorias from "@/components/Categorias";
 import Facilidades from "@/components/Facilidades";
 import Novidades from "@/components/Novidades";
 
-const Home = ({
-  carrinho,
-  adicionarProduto,
-  removerProduto,
-  removerProdutoCarrinho,
-  valorTotalCarrinho,
-  quantidadeProdutos,
-}) => {
+const Home = () => {
   return (
     <>
-      <BarraNavegacao quantidadeProdutos={quantidadeProdutos} />
-      <CarrinhoSuspenso
-        carrinho={carrinho}
-        adicionarProduto={adicionarProduto}
-        removerProduto={removerProduto}
-        removerProdutoCarrinho={removerProdutoCarrinho}
-        valorTotalCarrinho={valorTotalCarrinho}
-      />
+      <BarraNavegacao   />
+      <CarrinhoSuspenso />
       <main>
-        <Carrossel />
-        <Categorias />
-        <Produtos adicionarProduto={adicionarProduto} />
+        <Carrossel   />
+        <Categorias  />
+        <Produtos    />
         <Facilidades />
-        <Novidades />
+        <Novidades   />
       </main>
       <Rodape />
     </>
