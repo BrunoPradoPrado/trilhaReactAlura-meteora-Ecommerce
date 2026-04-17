e19fa05403d66da0d584fd74d7e2664ebea3aa2b
brunopradoprado <bruno.2006p@gmail.com>
2026-04-17 08:39:20 -0300

    desafio criar function removerProduto

--- src/components/CarrinhoSuspenso/index.jsx
+++ src/components/CarrinhoSuspenso/index.jsx
@@ -1,16 +1,12 @@
-import React from "react";
-import Botao from "@/components/Botao";
+import React, { useContext } from "react";
+import Botao                 from "@/components/Botao";
 import ListaProdutosCarrinho from "@/components/ListaProdutosCarrinho";
-import TotalCarrinho from "./TotalCarrinho";
-import Titulo from "@/components/Titulo";
+import TotalCarrinho         from "./TotalCarrinho";
+import Titulo                from "@/components/Titulo";
+import { CarrinhoContext }   from "@/context/CarrinhoContext";
 
-const CarrinhoSuspenso = ({
-  carrinho,
-  adicionarProduto,
-  removerProduto,
-  removerProdutoCarrinho,
-  valorTotalCarrinho,
-}) => {
+const CarrinhoSuspenso = () => {
+  const {carrinho} = useContext(CarrinhoContext)
   return (
     <div
       className="offcanvas offcanvas-end text-bg-dark"
@@ -31,16 +27,11 @@ const CarrinhoSuspenso = ({
           type="button"
           data-bs-dismiss="offcanvas"
           aria-label="Close"
-        ></Botao>
+        ></Botao>   
       </div>
       <div className="offcanvas-body">
-        <ListaProdutosCarrinho
-          carrinho={carrinho}
-          adicionarProduto={adicionarProduto}
-          removerProduto={removerProduto}
-          removerProdutoCarrinho={removerProdutoCarrinho}
-        />
-        <TotalCarrinho valorTotalCarrinho={valorTotalCarrinho} />
+        <ListaProdutosCarrinho carrinho = {carrinho} />
+        {/* <TotalCarrinho valorTotalCarrinho={valorTotalCarrinho} /> */}
       </div>
     </div>
   );
--- src/components/ListaProdutosCarrinho/index.jsx
+++ src/components/ListaProdutosCarrinho/index.jsx
@@ -1,14 +1,9 @@
-import React from "react";
+import React                from "react";
 import ItemCarrinhoSuspenso from "@/components/CarrinhoSuspenso/ItemCarrinhoSuspenso";
-import ItemCarrinho from "@/components/ItemCarrinho";
-import { useLocation } from "react-router-dom";
+import ItemCarrinho         from "@/components/ItemCarrinho";
+import { useLocation }      from "react-router-dom";
 
-const ListaProdutosCarrinho = ({
-  carrinho,
-  adicionarProduto,
-  removerProduto,
-  removerProdutoCarrinho,
-}) => {
+const ListaProdutosCarrinho = ({ carrinho }) => {
   const location = useLocation();
   return (
     <ul className="list-unstyled">
@@ -18,19 +13,13 @@ const ListaProdutosCarrinho = ({
         carrinho.map((itemCarrinho) => {
           return location.pathname === "/carrinho" ? (
             <ItemCarrinho
-              key={itemCarrinho.id}
-              itemCarrinho={itemCarrinho}
-              adicionarProduto={adicionarProduto}
-              removerProduto={removerProduto}
-              removerProdutoCarrinho={removerProdutoCarrinho}
+              key          = {itemCarrinho.id}
+              itemCarrinho = {itemCarrinho}
             />
           ) : (
             <ItemCarrinhoSuspenso
-              key={itemCarrinho.id}
-              itemCarrinho={itemCarrinho}
-              adicionarProduto={adicionarProduto}
-              removerProduto={removerProduto}
-              removerProdutoCarrinho={removerProdutoCarrinho}
+              key          = {itemCarrinho.id}
+              itemCarrinho = {itemCarrinho}
             />
           );
         })
